24c24b623faf40973738c7c1640fb9494fbfee1b
Raghu Betina <rbetina@gmail.com>
2013-08-28 11:00:58 -0500

    Added a form for a new note to the index page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -28,7 +28,7 @@ class NotesController < ApplicationController
 
     respond_to do |format|
       if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
+        format.html { redirect_to notes_url, notice: 'Note was successfully created.' }
         format.json { render action: 'show', status: :created, location: @note }
       else
         format.html { render action: 'new' }
